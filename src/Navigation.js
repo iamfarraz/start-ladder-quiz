6359929659f5930f37e49e5863558d4a5ec14338
Faraz <farazanwar8@gmail.com>
2022-01-01 16:37:09 +0530

    Fully functioning Nav bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,26 +1,39 @@
 import "./Navigation.css"
 import logo from "./logo.png"
+import {useState} from 'react'
 
 function Navigation() {
+   const [nav,setnav]=useState('');
+   const [burger,setburger]=useState('');
   return (
-    
   <header className="container">
        
    <nav className="nav">
        <div id="brand">
-           <img src={logo} />
+           <img src={logo} alt="logo"/>
        </div>
-     <ul className="list">
+     <ul className={`list ${nav}`}>
       <li><a href="#">Programs</a></li>
       <li><a href="#">Live Projects</a></li>
       <li><a href="#">Community</a></li>
       <li><a href="#">Jobs</a></li>
       <li><a href="#">About</a></li>
      </ul>
-     <div className="burger">
-         <div className="line1"></div>
-         <div className="line2"></div>
-         <div className="line3"></div>
+     <div className="burger" 
+        onClick={()=>{
+            //showing the navbar content
+           if(nav==='') setnav('list-active')
+           else setnav('')
+
+           // Animating Burger
+           if(burger==='') setburger('cross')
+           else setburger('')
+            
+        } 
+        }>
+         <div className={`line1-${burger}`}></div>
+         <div className={`line2-${burger}`}></div>
+         <div className={`line3-${burger}`}></div>
      </div>
    </nav>
     </header>
