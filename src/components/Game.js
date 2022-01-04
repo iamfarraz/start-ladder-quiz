0399b1c245ee4741d88c449b157cfc599f902e41
Faraz <farazanwar8@gmail.com>
2022-01-04 13:40:39 +0530

    Question rectangle grid done

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,24 @@
 import './Game.css'
 import Start from "./Start"
 import End from "./End"
+import Question from './Question';
 function Game() {
   //start
   //questions
   //end
   return (
     <div className="Game">
-      {/* <Start/>   */}
-        <End/>
+      {/* <div className='start game'>
+             <Start/>  
+      </div> */}
+      <div className='question game'>
+      <Question/>
+      </div>
+     
+      {/* <div className='end game'>
+         <End/>
+      </div> */}
+       
     </div>
   );
 }
---
+++ src/components/Question.js
@@ -0,0 +1,16 @@
+import './Question.css'
+
+function Question() {
+  
+  return (
+    <div className="Question ">
+      <div className='topic'></div>
+      <div className='question_no'></div>
+      <div className='answer'></div>
+
+        
+    </div>
+  );
+}
+
+export default Question;
